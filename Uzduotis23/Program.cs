69986b2943e49c204f12cf010abc8eb7a15eb9e5
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-10-25 17:34:29 +0300

    Uzdavinys23_Masyvai

---
+++ Uzduotis23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Uzduotis23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Keliones
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            double degal = 5.5; // litrai 100-ui km
+            double[] km = { 80, 42.8, 74.7, 14.8, 17, 23, 9 };
+            double min = km.Min();
+            Console.WriteLine($"Trumpiausias nuvažiuotas atstumas: {min} km");
+            double max = km.Max();
+            Console.WriteLine($"Ilgiausias nuvažiuotas atstumas: {max} km");
+            double suma = 0;
+            for ( int i = 0; i < km.Length; i++ )
+            {
+                suma += km[i];
+            }
+            Console.WriteLine($"Kuras, sunaudotas, nuvažiavus visus atstumas: {5.5*100/suma} litrai");
+
+
+        }
+    }
+}
