b2db084aca48105fb009397415ffd5ad5afd8fff
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-10-28 00:04:58 +0300

    Uzdavinys27

---
+++ Uzduotis27/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            List<string> zodziai = new List<string> { "stalas", "sofa", "spinta", "spintelė", "komoda" };
+            string ilgZodis = zodziai.OrderByDescending(w => w.Length).First();
+            Console.WriteLine($"Ilgiausias žodis:   {ilgZodis},  jis sudarytas iš: {ilgZodis.Length} raidžių");
+            string trumpZodis = zodziai.OrderByDescending(w => w.Length).Last();
+            Console.WriteLine($"Trumpiausias žodis:  {trumpZodis}, jis sudarytas iš: {trumpZodis.Length} raidžių");
+        }
+    }
+}
