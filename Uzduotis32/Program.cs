2edc6a8b19309fb1400dc356e1a5a0f0ff6f50ca
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-10-29 14:47:55 +0200

    Uzdavinys32_Metodai

---
+++ Uzduotis32/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uzduotis32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write("Įveskite savo vardą: ");
+            string vardas = Console.ReadLine();
+            Console.WriteLine();
+            PasisveikinimasSuVardu(vardas);  
+        }
+        static void PasisveikinimasSuVardu(string vardas)
+        {
+            Console.WriteLine("Labas " + vardas);
+        }
+    }
+}
